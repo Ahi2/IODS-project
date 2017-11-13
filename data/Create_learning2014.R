2c0ff4eec097a5ceb1331d4c89c94b351ea65aae
Mohamadali Ahi <Mohamadali.Ahi@lut.fi>
2017-11-13 13:08:15 +0200

    structure and dimensions

--- data/Create_learning2014.R
+++ data/Create_learning2014.R
@@ -1,2 +1,5 @@
 # Ali, 13.11.2017, exercise2_DataWrangling
 lrn14 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+dim(lrn14)
+str(lrn14)
+# 183 rows and 60 columns 
