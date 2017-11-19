bb5cde137d3450eacd3eeb1863db8ce6097d4ee3
Ali231 <mohamadali.ahi@helsinki.fi>
2017-11-19 12:39:32 +0200

    Reading and exploring data-2

--- data/create_alc.R
+++ data/create_alc.R
@@ -1,12 +1,12 @@
 My name is: Ali Ahi; date: 19.11.2017; 
 This is the third exercise of the course, focused on logistic regression.
 
-studentmat <- read.table("C:/Data/IODS-project/student-mat.csv", sep=";", header=TRUE)
+math <- read.table("C:/Data/IODS-project/student-mat.csv", sep=";", header=TRUE)
 
-dim(studentmat)
-str(studentmat)
+dim(math)
+str(math)
 
-studentpor <- read.table("C:/Data/IODS-project/student-por.csv", sep=";", header=TRUE)
+por <- read.table("C:/Data/IODS-project/student-por.csv", sep=";", header=TRUE)
 
-dim(studentpor)
-str(studentpor)
+dim(por)
+str(por)
