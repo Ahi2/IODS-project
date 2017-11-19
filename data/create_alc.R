5d5a6bc0bf284fcde176d2689ae8bd67d41c4591
Ali231 <mohamadali.ahi@helsinki.fi>
2017-11-19 12:50:24 +0200

    Joining the two dataset

--- data/create_alc.R
+++ data/create_alc.R
@@ -10,3 +10,11 @@ por <- read.table("C:/Data/IODS-project/student-por.csv", sep=";", header=TRUE)
 
 dim(por)
 str(por)
+
+library(dplyr)
+
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
+
+colnames(math_por)
+glimpse(math_por)
